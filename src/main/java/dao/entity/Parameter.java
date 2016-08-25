3fc4435d12b5a7d08204755032acec3dcd00ce37
hazerFluid <euroshow.light.demyanov@gmail.com>
2016-08-25 12:09:39 +0400

    getters/setters/equals/hashcode methods added to entities

--- src/main/java/dao/entity/Parameter.java
+++ src/main/java/dao/entity/Parameter.java
@@ -1,6 +1,7 @@
 package dao.entity;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "parameter") //
@@ -25,13 +26,28 @@ public class Parameter {
 
     //<editor-fold desc="GET/SET">
 
-
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
     }
 
     public float getWeight() {
@@ -49,10 +65,14 @@ public class Parameter {
     public void setVolume(float volume) {
         this.volume = volume;
     }
+
     //</editor-fold>
 
     //<editor-fold desc="Constructor">
-    public Parameter(float weight, float volume) {
+
+    public Parameter(Product product, String brand, float weight, float volume) {
+        this.product = product;
+        this.brand = brand;
         this.weight = weight;
         this.volume = volume;
     }
@@ -60,4 +80,22 @@ public class Parameter {
     public Parameter() {
     }
     //</editor-fold>
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Parameter parameter = (Parameter) o;
+        return id == parameter.id &&
+                Float.compare(parameter.weight, weight) == 0 &&
+                Float.compare(parameter.volume, volume) == 0 &&
+                Objects.equals(product, parameter.product) &&
+                Objects.equals(brand, parameter.brand);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, product, brand, weight, volume);
+    }
 }
--- src/main/java/dao/entity/Product.java
+++ src/main/java/dao/entity/Product.java
@@ -2,6 +2,7 @@ package dao.entity;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "product")
@@ -14,25 +15,25 @@ public class Product {
 
     @OneToOne
     @JoinColumn(name = "parameter")
-    Parameter parameter;
+    private Parameter parameter;
 
     @ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY) //
     @JoinTable(name = "products_has_categories",
             joinColumns = @JoinColumn(name = "productID"),
             inverseJoinColumns = @JoinColumn(name = "categoryID"))
-    List<Category> categories;
+    private List<Category> categories;
 
     @ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY) //
     @JoinTable(name = "carts_has_products",
             joinColumns = @JoinColumn(name = "productID"),
             inverseJoinColumns = @JoinColumn(name = "cartID"))
-    List<Cart> carts;
+    private List<Cart> carts;
 
     @ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY) //
     @JoinTable(name = "orrder_has_products",
             joinColumns = @JoinColumn(name = "productID"),
             inverseJoinColumns = @JoinColumn(name = "orrderID"))
-    List<Order> orrders;
+    private List<Order> orrders;
 
     @Column(name = "name")
     private String name;
@@ -42,13 +43,92 @@ public class Product {
 
     //<editor-fold desc="GET/SET">
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Parameter getParameter() {
+        return parameter;
+    }
+
+    public void setParameter(Parameter parameter) {
+        this.parameter = parameter;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public List<Cart> getCarts() {
+        return carts;
+    }
+
+    public void setCarts(List<Cart> carts) {
+        this.carts = carts;
+    }
+
+    public List<Order> getOrrders() {
+        return orrders;
+    }
+
+    public void setOrrders(List<Order> orrders) {
+        this.orrders = orrders;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getInStock() {
+        return inStock;
+    }
+
+    public void setInStock(int inStock) {
+        this.inStock = inStock;
+    }
+
     //</editor-fold>
 
     //<editor-fold desc="Constructor">
+
+    public Product(Parameter parameter, List<Category> categories, List<Cart> carts, List<Order> orrders, String name, int inStock) {
+        this.parameter = parameter;
+        this.categories = categories;
+        this.carts = carts;
+        this.orrders = orrders;
+        this.name = name;
+        this.inStock = inStock;
+    }
 
     public Product() {
     }
     //</editor-fold>
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id &&
+                Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
 
