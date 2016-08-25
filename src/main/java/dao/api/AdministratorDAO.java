14949b4afa013f2c604a63cb93813a761556baf0
hazerFluid <euroshow.light.demyanov@gmail.com>
2016-08-25 13:50:32 +0400

    minor changes

--- src/main/java/dao/api/AdministratorDAO.java
+++ src/main/java/dao/api/AdministratorDAO.java
@@ -1,4 +1,10 @@
 package dao.api;
 
+import dao.entity.Visitor;
+
 public interface AdministratorDAO {
+
+    Visitor getVisitorById(int id);
+    Visitor getVisitorByMail(String mail);
+    boolean isAdministrator(Visitor visitor);
 }
--- src/main/java/dao/api/CategoryDAO.java
+++ src/main/java/dao/api/CategoryDAO.java
@@ -1,4 +1,13 @@
 package dao.api;
 
+import dao.entity.Category;
+import java.util.List;
+
 public interface CategoryDAO {
+
+    void createCategory(Category category);
+    void updateCategory(Category category);
+    Category findCategoryById(Long id);
+    List<Category> findAllCategories();
+
 }
